0cd941b66832b9ce1d84e187aa0cb093d696dddf
knb58 <knb58@zips.uakron.edu>
2016-04-28 18:58:48 -0400

    Shows coordinates on the GUI.

---
+++ QuickHull/src/quickhull/QuickHull.java
@@ -0,0 +1,122 @@
+package quickhull;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import static java.awt.Component.CENTER_ALIGNMENT;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+
+/**
+ *
+ * @author Kirsten Baker, Cameron Reilly
+ */
+public class QuickHull extends JFrame {
+
+    public QuickHull() {
+
+        //create file input area
+        JLabel dir = new JLabel("Please input the path of the text file.");
+        JTextField fileField = new JTextField(20);
+        dir.setAlignmentX(CENTER_ALIGNMENT);
+        
+        //create graph size area
+        JTextField size = new JTextField(4);
+        size.setText("50");
+        size.setAlignmentX(CENTER_ALIGNMENT);
+        
+        JLabel sizeSet = new JLabel("Set graph size:");
+        sizeSet.setAlignmentX(CENTER_ALIGNMENT);
+        
+        JLabel sizeDefault = new JLabel("(Default is 50)");
+        sizeDefault.setFont(new Font("SANS_SERIF", Font.PLAIN, 10));
+        sizeDefault.setAlignmentX(CENTER_ALIGNMENT);
+        
+        //Submit button
+        JButton submitButton = new JButton("Submit");
+        submitButton.setAlignmentX(CENTER_ALIGNMENT);
+
+        //create a listener for the submit button
+        class ButtonListener implements ActionListener {
+
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                String file = fileField.getText();
+                int graph = Integer.parseInt(size.getText());
+                dispose(); //gets rid of the open file location gui  
+
+                //Create a frame to show the user
+                JFrame frame = new JFrame();
+                final int FRAME_WIDTH = 500;
+                final int FRAME_HEIGHT = 500;
+                frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+                frame.setTitle("Quick Hull");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLocationRelativeTo(null);
+                frame.setResizable(false);
+
+                coords2D coords = new coords2D(file, graph, FRAME_WIDTH, FRAME_HEIGHT);
+                String userCoords = coords.getCoordinates();
+
+                //Create a text field to show coordinates & scroll bar to show all of them
+                JTextField coordinates = new JTextField();
+                coordinates.setBorder(new LineBorder(new Color(0, 0, 0, 0)));
+                coordinates.setText(userCoords);
+                coordinates.setEditable(false);
+                JScrollPane co = new JScrollPane(coordinates);
+                co.setPreferredSize(new Dimension(FRAME_HEIGHT, 50));
+
+//                JButton showCoords = new JButton("Show Coordinates");
+//                JLabel showStep = new JLabel("Show Step: ");
+//                JTextField stepNo = new JTextField(5);
+//
+//                //add to panel
+//                JPanel show = new JPanel();
+//                show.add(showCoords);
+//                show.add(showStep);
+//                show.add(stepNo);
+
+                //frame.add(co, BorderLayout.NORTH);
+                frame.add(coords);
+               // frame.add(show, BorderLayout.SOUTH);
+                frame.setVisible(true);
+
+            }
+        }
+
+        //add listener to button
+        ActionListener listener = new ButtonListener();
+        submitButton.addActionListener(listener);
+
+        //add to panel 
+        JPanel fileLoc = new JPanel();
+        fileLoc.add(dir);
+        fileLoc.add(fileField);
+        fileLoc.add(sizeSet);
+        fileLoc.add(size);
+        fileLoc.add(sizeDefault);
+        fileLoc.add(submitButton, BorderLayout.SOUTH);
+
+        //add to the frame
+        add(fileLoc);
+    }
+
+    public static void main(String[] args) {
+        //create GUI
+        JFrame fileFrame = new QuickHull();
+        fileFrame.setTitle("Open the Text File");
+        fileFrame.setSize(250, 150);
+        fileFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fileFrame.setLocationRelativeTo(null);
+        fileFrame.setResizable(false);
+        fileFrame.setVisible(true);
+    }
+}
---
+++ QuickHull/src/quickhull/coords2D.java
@@ -0,0 +1,84 @@
+
+package quickhull;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import javax.swing.JComponent;
+
+public class coords2D extends JComponent {
+    
+    private boolean coords[][];
+    private int graphSize, buffer=20;
+    private double frameX, frameY;
+    private String coordinates="";
+
+    public coords2D(String file, int size, double frameXSize, double frameYSize) {
+        int xLoc, yLoc;
+        buffer=20;  graphSize=size+1;  
+        frameX=frameXSize-20;//-buffer;
+        frameY=frameYSize-40;//-85-buffer;
+        
+        coords= new boolean[graphSize][graphSize];
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(new File("C:\\Users\\dell\\Documents\\test.txt"));
+        } catch (FileNotFoundException ex) {}
+        
+        while (fileScanner.hasNext()){
+                xLoc=fileScanner.nextInt();
+                yLoc=fileScanner.nextInt();
+                coords[xLoc][yLoc] = true;
+                coordinates+="("+ xLoc +","+ yLoc + ") "; 
+        }
+       //  System.out.println(coordinates);
+    }
+    
+    public String getCoordinates(){
+        return coordinates;
+    }
+    
+    @Override
+    public void paintComponent(Graphics g){
+        //super.paintComponent(g);
+         Graphics2D g2 = (Graphics2D) g;
+         Rectangle2D graph;
+         Ellipse2D graphPoints;
+         double size=  graphSize;
+         int point=5;
+         
+//         graph= new Rectangle2D.Double(0, 0, frameX+buffer/2, frameY+buffer);
+//         g2.setStroke(new BasicStroke(2));
+//         g2.setColor(Color.LIGHT_GRAY);
+//         g2.draw(graph);
+//         
+//         Line2D xLine, yLine;
+//         for (int i=0; i<frameX+buffer/2; i+=1*point){
+//             g2.setStroke(new BasicStroke(1));
+//             xLine= new Line2D.Double(i, 0, i, frameY+buffer);
+//             for (int j=0; j<frameY+buffer; j+=1*point){
+//                yLine= new Line2D.Double(0, j ,frameX+buffer/2, j);
+//                g2.draw(yLine);
+//             }
+//             g2.draw(xLine);
+//         }
+         
+         
+          for (int i=0; i<coords.length;  i++)
+            for (int j=0; j<coords.length; j++) 
+                if(coords[i][j]){
+                    graphPoints= new Ellipse2D.Double(i*(frameX/size)+2*point, (-j+size)*(frameY/size),point,point);
+                    g2.setColor(Color.BLACK);
+                    g2.fill(graphPoints);
+                }
+    }
+    
+    
+}
